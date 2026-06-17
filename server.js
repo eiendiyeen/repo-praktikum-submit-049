13f00d13505a461c2c9bb77032d3ae2f0cf7ff3b
andieennn <andienswesty@gmail.com>
2026-06-17 10:57:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mysql = require('mysql2');
 const { BlobServiceClient } = require('@azure/storage-blob');
 const multer = require('multer');
-
 const upload = multer({ storage: multer.memoryStorage() });
 const app = express();
 
@@ -11,11 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Koneksi Database (Konfigurasinya di Azure Portal nanti)
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false }
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
 });
 
 // Koneksi Blob Storage
@@ -23,8 +22,8 @@ const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZU
 
 // Endpoint untuk submit tugas
 app.post('/submit-task', upload.single('file_tugas'), async (req, res) => {
-    const {nim, name, class_name, course} = req.body;
-    const blobName =`${nim}_${req.file.originalname}`;
+    const { nim, name, class_name, course } = req.body;
+    const blobName = `${nim}_${req.file.originalname}`;
 
     // 1. Upload ke Azure Blob Storage
     const containerClient = blobServiceClient.getContainerClient('tugas-praktikum');
@@ -33,12 +32,11 @@ app.post('/submit-task', upload.single('file_tugas'), async (req, res) => {
     const fileUrl = blockBlobClient.url;
 
     // 2. Simpan Metadata ke MySQL
-    const sql = "INSERT INTO submissions (nim, name, class, course, file_url)VALUES (?, ?, ?, ?, ?)";
-    db.query(sql, [nim, name, class_name, course, fileUrl],(err) => {
-            if (err)return res.status(500).send(err);
-            res.send("<h1>Tugas Berhasil Dikirim.</h1><a href='/'>Kembali</a>");
-        });
+    const sql = "INSERT INTO submissions (nim, name, class_name, course, file_url) VALUES (?, ?, ?, ?, ?)";
+    db.query(sql, [nim, name, class_name, course, fileUrl], (err) => {
+        if (err) return res.status(500).send(err);
+        res.send("<h1>Tugas Berhasil Dikirim!</h1><a href='/'>Kembali</a>");
+    });
 });
-app.listen(
-process.env.PORT || 3000
-);
+
+app.listen(process.env.PORT || 3000);
